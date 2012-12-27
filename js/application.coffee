ec78f8966e687625613a8929a0fc458653a9c126
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-27 17:36:04 +0530

    Add a class on li.tweet based on the number of Retweets.
    Also add underscore.js

--- js/application.coffee
+++ js/application.coffee
@@ -10,4 +10,21 @@ App.FlashbackController = ($scope, $http) ->
     $http.jsonp(url).
       success (response) ->
         $scope.tweets = response.results
+
+        max_retweets = _.max $scope.tweets, (tweet) ->
+          tweet.metadata.recent_retweets
+        .metadata.recent_retweets
+
+        _.each $scope.tweets, (tweet) ->
+          ratio = (tweet.metadata.recent_retweets || 0) / max_retweets
+          console.log ratio
+          tweet.class = if ratio < 0.25
+                          "one-fourth"
+                        else if ratio < 0.5
+                          "one-half"
+                        else if ratio < 0.75
+                          "three-fourths"
+                        else
+                          "one-whole"
+
         $scope.loading = false
--- js/application.js
+++ js/application.js
@@ -11,7 +11,17 @@
       query = $scope.query;
       url = "https://search.twitter.com/search.json?" + ("q=" + (encodeURIComponent(query)) + "&rpp=500&result_type=mixed&") + "callback=JSON_CALLBACK";
       return $http.jsonp(url).success(function(response) {
+        var max_retweets;
         $scope.tweets = response.results;
+        max_retweets = _.max($scope.tweets, function(tweet) {
+          return tweet.metadata.recent_retweets;
+        }).metadata.recent_retweets;
+        _.each($scope.tweets, function(tweet) {
+          var ratio;
+          ratio = (tweet.metadata.recent_retweets || 0) / max_retweets;
+          console.log(ratio);
+          return tweet["class"] = ratio < 0.25 ? "one-fourth" : ratio < 0.5 ? "one-half" : ratio < 0.75 ? "three-fourths" : "one-whole";
+        });
         return $scope.loading = false;
       });
     };
